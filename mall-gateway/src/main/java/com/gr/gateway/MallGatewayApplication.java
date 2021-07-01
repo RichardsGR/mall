d869b2708f5798e8318c105e13a11af42d244191
RichardGR <1349676288@qq.com>
2021-07-01 16:10:01 +0800

    新增gateway

--- mall-gateway/src/main/java/com/gr/gateway/MallGatewayApplication.java
+++ mall-gateway/src/main/java/com/gr/gateway/MallGatewayApplication.java
@@ -2,8 +2,15 @@ package com.gr.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@SpringBootApplication
+/**
+ *
+ * 开启服务注册发现（配置注册中心地址）
+ */
+@EnableDiscoveryClient
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class MallGatewayApplication {
 
     public static void main(String[] args) {
